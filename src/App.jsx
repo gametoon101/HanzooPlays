7b8ee4ba698561685107d21db8beb0ce605ef97d
gametoon101 <gametoon101@gmail.com>
2024-08-25 09:39:56 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Carousel from "./components/Carousel";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Carousel />
     </div>
   );
 }
